5fd302aaad2a479934611497e4d7bda8e57d30d7
yuj15 <yuj15@rpi.edu>
2024-02-16 17:15:19 -0500

    Projectile starter
    I added a projectile. need to brush up on the movement of it

--- fish game/pew_pew.gd
+++ fish game/pew_pew.gd
@@ -6,3 +6,13 @@ func _physics_process(delta):
 	if enemies_in_range.size() > 0: 
 		var target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
+
+
+func shoot():
+	const BULLET = preload("res://projectile.tscn")
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_position = %pewpod.global_position
+	%pewpod.add_child(new_bullet)
+
+func _on_timer_timeout():
+	shoot()
